2e3e61e269cc04c7410a4003be2bfa73d4c0d0f0
unknown <yewrui@hotmail.com>
2020-11-29 12:35:43 +0800

    Fix Main Page Responsive Issues - 3

--- src/components/UI/CardSlider/CardSlider.js
+++ src/components/UI/CardSlider/CardSlider.js
@@ -4,8 +4,30 @@ import classes from '../../UI/CardSlider/CardSlider.module.css';
 import styled from 'styled-components';
 import NasiLemakImg from '../../../assets/images/nasi_lemak_sample.jpg';
 
+
+function NoneNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "none"}}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function NonePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "none"}}
+        onClick={onClick}
+      />
+    );
+  }
+
 class cardSlider extends Component {
-    
     render()
         {
             const settings = {
@@ -16,6 +38,15 @@ class cardSlider extends Component {
                 slidesToScroll: 4,
                 responsive: [
                 {
+                    breakpoint: 1200,
+                    settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                    }
+                },
+                {
                     breakpoint: 1024,
                     settings: {
                     slidesToShow: 2,
@@ -25,18 +56,32 @@ class cardSlider extends Component {
                     }
                 },
                 {
+                    breakpoint: 768,
+                    settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                    nextArrow: <NoneNextArrow />,
+                    prevArrow: <NonePrevArrow />
+                    }
+                },
+                {
                     breakpoint: 600,
                     settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    initialSlide: 1
+                    initialSlide: 1,
+                    nextArrow: <NoneNextArrow />,
+                    prevArrow: <NonePrevArrow />
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    nextArrow: <NoneNextArrow />,
+                    prevArrow: <NonePrevArrow />
                     }
                 }
                 ]
@@ -52,12 +97,10 @@ class cardSlider extends Component {
                     .slick-next:before{
                         color:black;
                         font-size:50px;
-
-                }
+                    },
                     .slick-track{
                         margin:auto;
                     }
-                
                 `
 
         return(
