4769745765a423c9ce119ead5d35c6b00514a138
Ng Yew Rui <yewrui.ng@NeoAsia.local>
2020-11-30 12:39:48 +0800

    Fix Main Page Responsive Issues - 5

--- src/components/UI/CardSlider/CardSlider.js
+++ src/components/UI/CardSlider/CardSlider.js
@@ -36,8 +36,8 @@ class cardSlider extends Component {
                 dots: true,
                 infinite: true,
                 speed: 500,
-                slidesToShow: 4,
-                slidesToScroll: 4,
+                slidesToShow: 3,
+                slidesToScroll: 3,
                 responsive: [
                 {
                     breakpoint: 1200,
