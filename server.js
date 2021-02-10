a32144ee311b0275d2664e685f4917b5f163689c
unknown <yewrui@hotmail.com>
2021-02-11 01:03:23 +0800

    Test Public Env - 1

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const uri = "mongodb+srv://ahrui123:ahrui123@myprojectcluster.9c5nh.mongodb.net/
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));//build
 
 
 //connect to mongoose db
@@ -41,7 +41,7 @@ app.use('/api/fooder_type',FooderTypeRouter);
 app.use('/api/fooder_register',FooderRegisterRouter);
 
 app.get('*', function (req, res){
-  res.sendFile(path.join(__dirname+'/build/index.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));//'/build/index.html'
 })
 
 
